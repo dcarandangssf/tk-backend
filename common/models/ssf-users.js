7da1a89fa1c6d52f2f15ad7e03e2d627f4cce13d
dcarandangssf <dcarandang@softstackfactory.org>
2016-11-01 23:09:29 +0000

    Integrating TKTest Backend: Part 1 (complete)

--- common/models/ssf-users.js
+++ common/models/ssf-users.js
@@ -1,16 +1,16 @@
 'use strict';
 
-module.exports = function(Ssfusers) {
-    // SSFUsers.observe('after save', function(ctx, next) {
-    //     if (ctx.isNewInstance === true) {
-    //         var instance = ctx.instance;
-    //         instance.createAccessToken(1209600000, function(err, response) {
-    //             if (err === null) ctx.instance["token"] = response.id;
-    //             next();
-    //         });
-    //     }
-    //     else {
-    //         next();
-    //     }
-    // });
+module.exports = function(SSFUsers) {
+    SSFUsers.observe('after save', function(ctx, next) {
+        if (ctx.isNewInstance === true) {
+            var instance = ctx.instance;
+            instance.createAccessToken(1209600000, function(err, response) {
+                if (err === null) ctx.instance["token"] = response.id;
+                next();
+            });
+        }
+        else {
+            next();
+        }
+    });
 };
